652e677ddf253e39faca14a298224fee8a0292fc
tomy <atsimandiny@gmail.com>
2026-07-23 02:16:27 +0300

    Add GitHub Pages deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/drccdiana/",
+  base: "/rapport_drcc_diana/",
 });
